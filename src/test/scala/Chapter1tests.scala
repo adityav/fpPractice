5571a53019484debd89487c7da395e68cf510a09
Aditya Vishwakarma <aditya@sunglass.io>
2013-11-05 14:40:26 +0530

    Separated into individual chapters

--- src/test/scala/Chapter1tests.scala
+++ src/test/scala/Chapter1tests.scala
@@ -1,10 +1,10 @@
 /**
- * Created with IntelliJ IDEA.
- * User: aditya
+ * User: Aditya Vishwakarma
  * Date: 4/11/13
  * Time: 7:26 PM
- * To change this template use File | Settings | File Templates.
+ * Note:
  */
+
 
 import org.specs2._
 class Chapter1tests extends Specification  {
@@ -19,55 +19,17 @@ class Chapter1tests extends Specification  {
   def e1 = {
     val l1 = List(1, 2, 3, 4, 5).reverse
     val l2 = List('a', 'b','c','d').reverse
-    Chapter1.isSorted(l1, (a:Int,b:Int) => a > b) and Chapter1.isSorted(l2, (a:Char,b:Char) => a > b)
+    Chapter2.isSorted(l1, (a:Int,b:Int) => a > b) and Chapter2.isSorted(l2, (a:Char,b:Char) => a > b)
   }
 
   def e2 = {
     val l1 = List(1, 6, 3, 4, 5).reverse
     val l2 = List('a', 'j','c','d').reverse
-    !Chapter1.isSorted(l1, (a:Int,b:Int) => a > b) and !Chapter1.isSorted(l2, (a:Char,b:Char) => a > b)
+    !Chapter2.isSorted(l1, (a:Int,b:Int) => a > b) and !Chapter2.isSorted(l2, (a:Char,b:Char) => a > b)
   }
 
-  def e3 = Chapter1.isSorted[Int](List.empty[Int], _ > _)
-  def e4 = Chapter1.isSorted[Int](List(1), _ > _)
+  def e3 = Chapter2.isSorted[Int](List.empty[Int], _ > _)
+  def e4 = Chapter2.isSorted[Int](List(1), _ > _)
 
 }
 
-class Chapter4tests extends Specification  {
-  def is = s2"""
-  Tests for Chapter 4 Example code
-
-  map2 should
-    option + option => Option $m1
-    option + None => None     $m2
-    none + none => None       $m3
-
-
-  traverse should
-    Some[List] for valid list      $e1
-    None for any other       $e2 """
-
-  def map2func[A,B](a:A ,b: B) = a :: b :: Nil
-
-  def m1 = {
-    Chapter4.map2(Some('a'), Some(1))(map2func) mustEqual Some('a' :: 1 :: Nil)
-  }
-  def m2 = {
-    Chapter4.map2(Some('a'), None:Option[Int])(map2func).isEmpty and
-    Chapter4.map2(None:Option[Char], Some(1))(map2func).isEmpty
-  }
-  def m3 = {
-    Chapter4.map2(None:Option[Char], None:Option[Int])(map2func).isEmpty
-  }
-
-
-  def e1 = {
-    val l1 = List(1, 2, 3, 4, 5)
-    Chapter4.traverse(l1)(a => Some(a)) mustEqual Some(l1)
-  }
-
-  def e2 = {
-    val l1 = List(1, 6, 3, 4, 5)
-    Chapter4.traverse(l1)(a => if(a == 6) None else Some(a)).isEmpty
-  }
-}
