3e31829655bfc93270420f79194c42b7880f74dd
Aditya Vishwakarma <aditya@sunglass.io>
2013-11-06 15:32:07 +0530

    finished Chapter 5

--- src/main/scala/Chapter5.scala
+++ src/main/scala/Chapter5.scala
@@ -8,7 +8,9 @@ import scala.annotation.tailrec
  */
 object Chapter5 {
   trait Stream[+A] {
-    def uncons: Option[(A, Stream[A])]
+    import Stream._
+
+    def uncons: Option[Cons[A]]
     def isEmpty: Boolean = uncons.isEmpty
 
     //Ex1 to List
@@ -30,9 +32,9 @@ object Chapter5 {
       val buf = collection.mutable.ListBuffer.empty[A]
       @tailrec
       def _tl(s:Stream[A]):List[A] = s.uncons match {
-        case Some((hd,tl)) =>
-          buf += hd
-          _tl(tl)
+        case Some(c) =>
+          buf += c.head
+          _tl(c.tail)
         case None => buf.toList
       }
       _tl(this)
@@ -41,7 +43,7 @@ object Chapter5 {
     //Ex2
     def take(n: Int): Stream[A] = if (n > 0) {
       uncons match {
-        case Some((hd, tl)) => Stream.cons(hd, tl.take(n - 1))
+        case Some(c) => Stream.cons(c.head, c.tail.take(n - 1))
         case None => Stream.empty[A]
       }
     } else Stream.empty[A]
@@ -50,24 +52,138 @@ object Chapter5 {
     def takeWhile(p: A => Boolean):Stream[A] = {
       def _tw(s:Stream[A]):Stream[A] = {
         s.uncons match {
-          case Some((hd, tl)) if p(hd) => Stream.cons(hd, _tw(tl))
+          case Some(c) if p(c.head) => Stream.cons(c.head, _tw(c.tail))
           case _ => Stream.empty[A]
         }
       }
       _tw(this)
     }
+
+    //Ex4
+    def forall(p: A => Boolean): Boolean = foldRight(true)((a, acc) => p(a) && acc)
+
+    //Ex5 takeWhile using FoldRight
+    def takeWhile2(p: A => Boolean): Stream[A] = {
+      foldRight(empty[A])((a, acc) => if(p(a)) cons(a, acc) else empty)
+    }
+    //Ex6
+    def map[B](f: A => B): Stream[B] = foldRight(empty[B])((h,t) => cons(f(h),t))
+    def filter(f: A => Boolean):Stream[A] = foldRight(empty[A]){
+      (h,t) =>
+        if(f(h)) cons(h, t) else t
+    }
+    def append[B >: A](s: Stream[B]):Stream[B] = foldRight(s)((h,t) => cons(h,t))
+    def flatMap[B](f: A => Stream[B]):Stream[B] = foldRight(empty[B])((h,t) => f(h) append t)
+
+
+    //Misc
+    def foldRight[B](z:B)(f:(A, => B) => B):B = uncons match {
+      case Some(c) => f(c.head, c.tail.foldRight(z)(f))
+      case _ => z
+    }
+    def exists(p: A => Boolean):Boolean = foldRight(false)((a, acc) => p(a) || acc)
+
+    //Using unfold
+    //Ex12
+    def mapU[B](f: A => B):Stream[B] = unfold(this)(_.uncons match {
+      case Some(c) => Some((f(c.head), c.tail))
+      case _ => None
+    })
+
+    def takeU(n:Int) = unfold((n, this))(n => if(n._1 == 0) None else {
+      n._2.uncons match {
+        case Some(c) => Some(c.head, (n._1 - 1, c.tail))
+        case _ => None
+      }
+    })
+    def takeWhileU(p: A => Boolean) = unfold(this)( _.uncons match {
+      case Some(c) if p(c.head) => Some(c.head, c.tail)
+      case _ => None
+    })
+
+    def zip[B](s2:Stream[B]): Stream[(A,B)] = unfold((this, s2)){
+      case (s1, s2) =>
+        (s1.uncons, s2.uncons) match {
+          case (Some(c1), Some(c2)) => Some((c1.head,c2.head), (c1.tail, c2.tail))
+          case _ => None
+        }
+    }
+    def zipAll[B](s2: Stream[B]):Stream[(Option[A], Option[B])] = {
+      val a = this map (Some(_)) append constant(None)
+      val b = s2 map (Some(_)) append constant(None)
+      a.zip(b)
+    }
+
+    //Ex14
+    def tails:Stream[Stream[A]] = unfold(this)(_.uncons match {
+      case Some(c) => Some(c, c.tail)
+      case _ => None
+    }) append Stream.empty
+
+    //Ex15
+    def scanRight[B](z:B)(f:(A, => B) => B): Stream[B] =
+    foldRight((z, Stream(z)))((a, p) => {
+      val b = f(a, p._1)
+      (b, cons(b, p._2))
+    })._2
+
+    def list5 = this.take(5).toList
+  }
+
+  object Empty extends Stream[Nothing] {
+    val uncons = None
+  }
+  sealed abstract class Cons[+A] extends Stream[A] {
+    def head: A
+    def tail: Stream[A]
+    val uncons = Some(this)
   }
 
   object Stream {
-    def empty[A]: Stream[A] =
-      new Stream[A] { def uncons = None }
-    def cons[A](hd: => A, tl: => Stream[A]): Stream[A] =
-      new Stream[A] {
-        lazy val uncons = Some((hd, tl))
+    def empty[A]: Stream[A] = Empty
+
+    def cons[A](hd: => A, tl: => Stream[A]): Stream[A] = new Cons[A] {
+        lazy val head = hd
+        lazy val tail = tl
       }
     def apply[A](as: A*): Stream[A] =
       if (as.isEmpty) empty
       else cons(as.head, apply(as.tail: _*))
-  }
 
+    val ones:Stream[Int] = cons(1, ones)
+
+    //ex7
+    def constant[A](a:A): Stream[A] = new Cons[A] {
+      lazy val head = a
+      lazy val tail = this
+    }
+    //ex8 infinite stream of nos
+    def from(n:Int):Stream[Int] = cons(n, from(n + 1))
+
+    //ex9
+    def fibs = {
+      def go(a: Int, b: Int):Stream[Int] = cons(a, go(b, a + b))
+      go(0,1)
+    }
+
+    //ex10
+    def unfold[A,S](z:S)(f: S => Option[(A,S)]):Stream[A] = {
+      f(z) match {
+        case Some((h,t)) => cons(h, unfold(t)(f))
+        case _ => empty[A]
+      }
+    }
+
+    //Using Unfold
+    //cons/ from/ fibs are easy
+
+    //Ex13
+    def startsWith[A](s:Stream[A], s2:Stream[A]):Boolean =
+    s.zipAll(s2).takeWhile(_._2.isDefined).forall{
+      case (Some(h), Some(h2)) if h == h2 => true
+      case _ => false
+    }
+
+
+  }
 }
--- src/test/scala/Chapter5tests.scala
+++ src/test/scala/Chapter5tests.scala
@@ -13,6 +13,14 @@ class Chapter5tests extends Specification {
       toList convert stream to a List $s1
       take(n) should take 1st n elements $s2
       takeWhile(p) should work        $s3
+      ones and constant work $s4
+      stream of numbers $s5
+      fibonacci series $s6
+      unfold works $s7
+
+    Ex13 onwards
+      startsWith works  $s8
+      tails works       $s9
     """
   import Chapter5._
   val l = (1 to 5).toList
@@ -24,5 +32,24 @@ class Chapter5tests extends Specification {
   def s3 = Stream(l: _*).takeWhile( _ < 3).toList mustEqual (1 to 2).toList and
     (Stream(l: _*).takeWhile( _ < 10).toList mustEqual l) and
     (Stream(l: _*).takeWhile( _ > 10).toList mustEqual List.empty)
+
+  def s4 = Stream.ones.list5 mustEqual List(1, 1, 1, 1, 1) and
+    (Stream.constant(1).list5 mustEqual Stream.ones.list5)
+
+  def s5 = Stream.from(3).list5 mustEqual (3 to 7).toList
+
+  def s6 = Stream.fibs.take(7).toList mustEqual List(0, 1, 1, 2, 3, 5, 8)
+
+  def s7 = Stream.unfold(3)(x => Some(x, x + 1)).list5 mustEqual Stream.from(3).list5
+
+  def s8 = {
+    val s = Stream(l: _*)
+    Stream.startsWith(s, Stream(1,2)) and !Stream.startsWith(s, Stream(2,3)) and
+      !Stream.startsWith(Stream(1,2), s) and !Stream.startsWith(Stream(2,3), s)
+  }
+
+  def s9 = {
+    Stream(1,2,3).tails.map(_.toList).toList mustEqual List(List(1, 2, 3), List(2, 3), List(3))
+  }
 
 }
